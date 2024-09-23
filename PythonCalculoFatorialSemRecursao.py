9345627801c852ec04c272994a833c751d3a52da
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-09-23 11:44:05 -0300

    Resolução de Calculo sem recursão

--- PythonCalculoFatorialSemRecursao.py
+++ PythonCalculoFatorialSemRecursao.py
@@ -1,2 +1,11 @@
 # Calculo do Fatorial(sem recursão)
 
+def fatorial(n):
+    soma = 1 
+    for i in range(1, n+1):
+        soma *= i 
+        return soma
+
+# Principal
+n = int(input('Número: '))
+print(f'Fatorial de {n} é {fatorial(n)}')
